1310b8f3187f72c69a567da27158cb29d8cfacaf
Daniel-Asare <johndoe@example.com>
2021-08-07 18:34:19 +0000

    k

---
+++ src/data.js
@@ -0,0 +1,88 @@
+const Burgers = [
+  {
+    id:1,
+    title:"Eggs Benedict Burger",
+    categories:"",
+    image:"https://images.unsplash.com/photo-1565299507177-b0ac66763828?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=364&q=80",
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam natus, cum neque numquam laboriosam maiores."
+
+  },
+  {
+    id:1,
+    title:"Juice Beef Burger Meal",
+    categories:"special",
+    price:16,
+    image:"https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/44/2020/09/Hue_Saturation-679-copy-3.jpg",
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam natus, cum neque numquam laboriosam maiores."
+
+  },
+  {
+    id:2,
+    title:"Choko Milkshake Frpm Heaven",
+    categories:"special",
+    price:8,
+    image:"https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/44/2020/09/Hue_Saturation-679-copy-4.jpg",
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam natus, cum neque numquam laboriosam maiores."
+  },
+  {
+    id:3,
+    title:"Blueberry Waffles",
+    categories:"special",
+    price:4,
+    image:"https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/44/2020/09/Hue_Saturation-679-copy.jpg",
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam natus, cum neque numquam laboriosam maiores."
+
+  },
+  {
+    id:4,
+    title:"Meduim Portion of Fries",
+    categories:"special",
+    price:9,
+    image:"https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/44/2020/09/Hue_Saturation-679-copy-2.jpg",
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam natus, cum neque numquam laboriosam maiores."
+
+  },
+  {
+    id:5,
+    title:"Sunday Waffle",
+    categories:"dessert",
+    price:8,
+    image:"https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/44/2020/09/dessert4.jpg",
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam natus, cum neque numquam laboriosam maiores."
+
+  },
+  {
+    id:6,
+    title:"Apple Pie",
+    categories:"dessert",
+    price:8,
+    image:"https://images.unsplash.com/photo-1565299507177-b0ac66763828?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=364&q=80",
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam natus, cum neque numquam laboriosam maiores."
+
+  },
+  {
+    id:7,
+    title:"Chocolate Mix",
+    categories:"dessert",
+    price:8,
+    image:"https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/44/2020/09/dessert1.jpg",
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam natus, cum neque numquam laboriosam maiores."
+
+  },
+  {
+    id:8,
+    title:"Strawberry Mix",
+    categories:"dessert",
+    price:8,
+    image:"https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/44/2020/09/dessert2.jpg",
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam natus, cum neque numquam laboriosam maiores."
+
+  }
+]
+
+
+
+
+
+
+export default Burgers
